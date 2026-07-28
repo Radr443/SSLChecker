c53813ab3d7be5ddcb86307493be1b2e2924bf33
Radr <radartomyheart@gmail.com>
2026-07-29 00:13:02 +0100

    Added Argument Support

--- sslchecker.py
+++ sslchecker.py
@@ -6,6 +6,7 @@ import termcolor
 import sys
 from colorama import init
 
+
 init()
 # Used to accept text files to read domains off 
 def check_file(filename):
@@ -87,27 +88,33 @@ def check_ssl(domain):
         )
 # MAIN
 try:
-
-    while True:
-        domains = input(
-            "Enter domains manually supported by commas to search for multiple domains\nor provide a txt file with domains (ex: domains.txt OR example.com,example.org): "
-        ).strip()
-        if domains.endswith(".txt"):
-            check_file(domains)
-        else:
-
-            domain_list = [
-                d.strip()
-                for d in domains.split(",")
-            ]
-            for domain in domain_list:
+    if len(sys.argv) > 1:
+        domains = sys.argv[1:]
+        for domain in domains:
+            if domain.endswith(".txt"):
+                check_file(domain)
+            else:
                 check_ssl(domain)
-        action = input(
-            "Type check to scan again or anything else to exit: "
-        )
+    else:
+        while True:
+            domains = input(
+                "Enter domains manually supported by commas to search for multiple domains\nor provide a txt file with domains (ex: domains.txt OR example.com,example.org): "
+            ).strip()
+            if domains.endswith(".txt"):
+                check_file(domains)
+            else:
+                domain_list = [
+                    d.strip()
+                    for d in domains.split(",")
+                ]
+                for domain in domain_list:
+                    check_ssl(domain)
+            action = input(
+                "Type check to scan again or anything else to exit: "
+            )
 
-        if action.lower() != "check":
-            sys.exit(0)
+            if action.lower() != "check":
+                sys.exit(0)
 
 except KeyboardInterrupt:
 
